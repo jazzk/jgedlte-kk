e8ac07502ad7993f453c6611ff19ba58b99eec44
jazzk <jkalsi@gmail.com>
2014-07-21 20:57:22 +0100

    Initial commit of Samsung GT-I9505G NF3 sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu May 15 18:28:01 KST 2014"
+#define LINUX_COMPILE_BY "omsgsm"
+#define LINUX_COMPILE_HOST "sy918-desktop"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
